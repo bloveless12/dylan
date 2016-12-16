b9730cf0fcf351788a77d73219c70416fa97593a
Braeden Loveless <lovey.b.love@aggiemail.usu.edu>
2016-12-16 10:11:37 -0700

    Add files via upload

---
+++ test_american.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+from dylan.payoff import VanillaPayoff, call_payoff, put_payoff
+from dylan.engine import BinomialPricingEngine, AmericanPricer
+from dylan.marketdata import MarketData
+from dylan.option import Option
+
+def main():
+    spot = 41.0
+    strike = 40.0
+    rate = 0.08
+    volatility = 0.30
+    expiry = 1.0
+    steps = 3
+    dividend = 0.0
+    
+    the_call = VanillaPayoff(expiry, strike, call_payoff)
+    the_bopm = BinomialPricingEngine(steps, AmericanPricer)
+    the_data = MarketData(rate, spot, volatility, dividend)
+    
+    the_option = Option(the_call, the_bopm, the_data)
+    fmt = "The call option price is {0:0.3f}"
+    print(fmt.format(the_option.price()))
+    
+    
+if __name__ == "__main__":
+    main()
+    
